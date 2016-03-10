da40d7f38af259b469183dcbd8cb655e8869d8fc
Mike Green <mike.is.green@gmail.com>
2016-03-10 16:48:21 -0500

    start annotating important sections in the webpack and uglify config modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,39 @@
+/**
+ * Webpack Test Project
+ * Webpack Configuration Module
+ *
+ * https://webpack.github.io/docs/configuration.html
+ */
+
 import webpack     from 'webpack';
 import gutil       from 'gulp-util';
 import path        from 'path';
 import uglifyConf  from './uglify-config';
 import ProgressBar from 'progress';
 
+// This will be true if --production is passed to gulp, false otherwise
 const production   = !!gutil.env.production;
+
 const node_modules = path.join(__dirname, 'node_modules');
 
+// Create a progress bar to display build status.
 let progress = new ProgressBar('[:bar] :percent', { total: 100 });
 
+// Update the progress bar using Webpack's progress plugin
 let progressPlugin = new webpack.ProgressPlugin((percent, msg) => {
   progress.update(percent);
 });
 
+// Create a vendor scripts bundle for common libraries using the
+// CommonsChunkPlugin.
+//
+// https://webpack.github.io/docs/list-of-plugins.html#commonschunkplugin
 let commonsChunkPlugin = new webpack.optimize.CommonsChunkPlugin({
   name: 'vendor',
   minChunks: Infinity
 });
 
+// Add copyright message to output files
 let bannerPlugin = new webpack.BannerPlugin('Copyright (C) 2016 ePublishing, Inc.');
 
 let webpackConfig = {
@@ -33,6 +49,8 @@ let webpackConfig = {
     alias: {}
   },
   entry: {
+    // Each output target needs an entry here that points Webpack to
+    // a source script.
     home: './source/js/home',
     page2: './source/js/page2',
     vendor: [
@@ -42,11 +60,12 @@ let webpackConfig = {
     ]
   },
   output: {
-    path: path.join(__dirname, 'build', 'js'),
-    filename: '[name].js'
+    path: path.join(__dirname, 'build', 'js'), // Output directory where compiled chunks will be saved
+    filename: '[name].js' // Placeholders usable here include [name] and [hash]
   },
   module: {
     loaders: [
+      // Load all JS files via Babel, compile ES2015 and React JSX to ES5
       {
         test: /\.jsx?$/,
         loader: 'babel',
@@ -58,6 +77,9 @@ let webpackConfig = {
           ]
         }
       },
+
+      // Compile sass files into JS modules. This allows React components
+      // to bundle their own stylesheets and load them via ES2015 imports.
       {
         test: /\.scss$/,
         loaders: [ 'style', 'css', 'sass' ]
@@ -65,13 +87,14 @@ let webpackConfig = {
     ]
   },
   plugins: [
-    bannerPlugin,
-    progressPlugin,
-    commonsChunkPlugin
+    bannerPlugin,      // Add copyright comment to the top of each output file
+    progressPlugin,    // Show a custom progress bar during build
+    commonsChunkPlugin // Create a vendor.js file with commonly used libraries instead of bundling them into each entry
   ]
 };
 
 if (production) {
+  // Minify output files. This adds several seconds to overall build time and results in much smaller files.
   webpackConfig.plugins.push(new webpack.optimize.UglifyJsPlugin(uglifyConf));
 }
 
